18814c4ad203ab94aa55d53e0690adddf20f7850
Sayakhatova <s.naz03@mail.ru>
2021-04-26 05:06:52 +0600

    .

--- TSIS_9/paint/a.py
+++ TSIS_9/paint/a.py
@@ -1,4 +1,4 @@
-import pygame, sys, time
+import pygame, sys
 
 pygame.init()
 
@@ -12,17 +12,11 @@ YELLOW=(255, 255, 0)
 ORANGE=(255, 140, 0)
 DARKBLUE=(0, 0, 139)
 
-prevPoint=(0, 0)
-curPoint=(0, 0)
-
-currentTool=0
-toolCount=4
-
-current_color=0
+color=BLACK
 colors=(RED, ORANGE, YELLOW, GREEN, BLUE, DARKBLUE, PURPLE, BLACK)
 
 screen=pygame.display.set_mode((800, 600))
-screen.fill(WHITE)
+screen.fill((255, 255, 255))
 pygame.display.set_caption('PAINT')
 
 e=pygame.image.load('eraser.png')
@@ -30,18 +24,28 @@ t=pygame.image.load('trash.png')
 c=pygame.image.load('circle.png')
 r=pygame.image.load('rect.png')
 
-def draw_rectangle(surface, color, x, y, h, w):
-    pygame.draw.rect(surface, color, (x, y, h, w), 5)
 def draw_rect(surface, color, x, y, h, w):
     pygame.draw.rect(surface, color, (x, y, h, w))
-def draw_circle(surface, color, x, y):
-    pygame.draw.circle(surface, color, (x, y), 30, 3)
-def draw_line(surface, color, startPos, endPos):
-    pygame.draw.line(surface, color, startPos, endPos, 2)
-def erase(surface, x, y):
-    pygame.draw.circle(surface, WHITE, (x, y), 30)
 
-isPressed=False
+red_rect=pygame.Rect(20, 10, 60, 30)
+orange_rect=pygame.Rect(20, 60, 60, 30)
+yellow_rect=pygame.Rect(20, 110, 60, 30)
+green_rect=pygame.Rect(20, 160, 60, 30)
+blue_rect=pygame.Rect(20, 210, 60, 30)
+darkblue_rect=pygame.Rect(20, 260, 60, 30)
+purple_rect=pygame.Rect(20, 310, 60, 30)
+black_rect=pygame.Rect(20, 360, 60, 30)
+rect_rect=pygame.Rect(20, 410, 60, 30)
+circle_rect=pygame.Rect(20, 460, 60, 30)
+eraser_rect=pygame.Rect(20, 510, 60, 30)
+trash_rect=pygame.Rect(20, 560, 60, 30)
+
+stamp_0=pygame.transform.scale(r, (60, 30))
+stamp_1=pygame.transform.scale(c, (60, 30))
+
+draw=False
+draw1=False
+stamp=0
 
 while True:
     
@@ -50,11 +54,11 @@ while True:
     for i in range(len(colors)):
         draw_rect(screen, colors[i], 20, (i*50+10), 60, 30)
         
-    screen.blit(r, pygame.Rect(20, 410, 60, 30)) 
-    screen.blit(c, pygame.Rect(20, 460, 60, 30))
-    screen.blit(e, pygame.Rect(20, 510, 60, 30))
-    screen.blit(t, pygame.Rect(20, 560, 60, 30))
-    
+    screen.blit(r, rect_rect) 
+    screen.blit(c, circle_rect)
+    screen.blit(e, eraser_rect)
+    screen.blit(t, trash_rect)
+
     pygame.display.update()
 
     for event in pygame.event.get():
@@ -65,31 +69,63 @@ while True:
             sys.exit()
 
         if event.type==pygame.KEYDOWN:
-            if event.key==pygame.K_n: 
-                currentTool=(currentTool+1)%toolCount
-            elif event.key==pygame.K_c:
-                current_color=(current_color+1)%len(colors)
-            elif event.key==pygame.K_e:
-                screen.fill(WHITE)
+            if event.key==pygame.K_ESCAPE:
+                pygame.quit()
+                sys.exit()
 
         if event.type==pygame.MOUSEBUTTONDOWN:
             if event.button==1:
-                isPressed=True
-                prevPoint=pygame.mouse.get_pos()
-        elif event.type==pygame.MOUSEBUTTONUP:
-            isPressed=False
-        elif event.type==pygame.MOUSEMOTION:
-            if isPressed:
-                prevPoint=curPoint
-                curPoint=pygame.mouse.get_pos()
+                draw=True
 
-    if currentTool==0:
-        draw_line(screen, colors[current_color], prevPoint, curPoint)
-    elif currentTool==1:
-        draw_rectangle(screen, colors[current_color], curPoint[0], curPoint[1], 50, 50)
-    elif currentTool==2:
-        draw_circle(screen, colors[current_color], *curPoint)
-    elif currentTool==3:
-        erase(screen, *curPoint)
+        if event.type==pygame.MOUSEBUTTONUP:
+            draw=False
+
+    mouse_pos=pygame.mouse.get_pos()
+    if draw and mouse_pos[0]>100:
+        if draw1==False: pygame.draw.circle(screen, color, mouse_pos, 5)
+        elif stamp==0 and draw1: 
+            screen.blit(stamp_0, (mouse_pos[0], mouse_pos[1]))
+            draw=False
+        elif stamp==1 and draw1:
+            screen.blit(stamp_1, (mouse_pos[0], mouse_pos[1]))
+            draw=False
+    
+    if draw:
+
+        if red_rect.collidepoint(mouse_pos): 
+            color=RED
+            draw1=False
+        if orange_rect.collidepoint(mouse_pos): 
+            color=ORANGE
+            draw1=False
+        if yellow_rect.collidepoint(mouse_pos): 
+            color=YELLOW
+            draw1=False
+        if green_rect.collidepoint(mouse_pos): 
+            color=GREEN
+            draw1=False
+        if blue_rect.collidepoint(mouse_pos): 
+            color=BLUE
+            draw1=False
+        if darkblue_rect.collidepoint(mouse_pos): 
+            color=DARKBLUE
+            draw1=False
+        if purple_rect.collidepoint(mouse_pos): 
+            color=PURPLE
+            draw1=False
+        if black_rect.collidepoint(mouse_pos): 
+            color=BLACK
+            draw1=False
+        if eraser_rect.collidepoint(mouse_pos): 
+            color=WHITE
+            draw1=False
+        if trash_rect.collidepoint(mouse_pos): draw_rect(screen, WHITE, 100, 0, 700, 600)
+
+        if rect_rect.collidepoint(mouse_pos): 
+            stamp=0 
+            draw1=True
+        if circle_rect.collidepoint(mouse_pos):
+            stamp=1
+            draw1=True
 
     pygame.display.update()
